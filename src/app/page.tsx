a6d5e31b842023f5463126ef95d229a8433c8a78
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2024-05-15 17:30:52 +0530

    gradient color added in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar user={true} />
-      <section className="h-screen w-full bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased"></section>
+      <section className="h-screen w-full bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]"></div>
+      </section>
     </main>
   );
 }
