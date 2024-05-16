c48e1b2a41fc0a8dcf19c765abb6b07209745628
Sagnik Ghosh <Sagnik.Ghosh@p360.com>
2024-05-16 17:19:25 -0400

    basic landing page creation done

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Sidebar from '@/components/sidebar'
+// import InfoBar from '@/components/infobar'
+
+type Props = { children: React.ReactNode }
+
+const Layout = (props: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+      <div className="w-full">
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout
