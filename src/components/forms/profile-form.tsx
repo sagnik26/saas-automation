9948b30cd0d1834535648bc79ed1c34c59fe0a58
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2024-05-20 15:56:49 +0530

    react-hook-form package added

---
+++ src/components/forms/profile-form.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState } from "react";
+
+type Props = {};
+
+const ProfileForm = (props: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  return <div>ProfileForm</div>;
+};
+
+export default ProfileForm;
